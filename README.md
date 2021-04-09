83c96078c764de826cb5f9dad8d8b3034a408495
bycop <thebycop@gmail.com>
2021-04-09 18:29:09 +0200

    Init

---
+++ README.md
@@ -0,0 +1,9 @@
+# FiveM Give weapon, spawncar, setmodel
+
+## To install
+- Download or clone this repository and create a folder named "weapon_model_car" in server-data/resources
+- Put __resource.lua and client.lua in server-data/resources/weapon_model_car
+- Add ensure weapon_model_car to your server.cfg
+- Now type /giveweapon weapon_key, /spawncar car_key or /setmodel model_key to use the script.<br>
+List of keys here https://wiki.rage.mp/index.php?title=Weapons https://wiki.rage.mp/index.php?title=Peds https://wiki.rage.mp/index.php?title=Vehicles <br><br>
+Preview here : https://streamable.com/zg2vh6
---
+++ client.lua
@@ -0,0 +1,57 @@
+function ShowNotification(text)
+    SetNotificationTextEntry("STRING")
+    AddTextComponentSubstringPlayerName(text)
+    DrawNotification(false, false)
+end
+
+RegisterCommand("giveweapon", function(source, args, rawCommand)
+	local playerPed = GetPlayerPed(-1)
+	local weaponHash = GetHashKey(args[1])
+	local ammoCount = 9999
+
+	GiveWeaponToPed(playerPed, weaponHash, ammoCount, false)
+end)
+
+RegisterCommand("setmodel", function(source, args, rawCommand)
+	local model = args[1]
+	local myPed = GetPlayerPed(-1)
+	local modelhashed = GetHashKey(model)
+	RequestModel(modelhashed)
+
+    Citizen.CreateThread(function() 
+		local waiting = 0
+		while not HasModelLoaded(modelhashed) do 
+			waiting = waiting + 100
+			RequestModel(modelhashed)
+			Citizen.Wait(100)
+			if waiting > 5000 then
+				ShowNotification("~r~Model not found.")
+                return
+			end
+		end
+		SetPlayerModel(PlayerId(), modelhashed)
+		SetModelAsNoLongerNeeded(modelhashed)
+		ShowNotification("~g~Your model has been changed.")
+	end)
+end)
+
+RegisterCommand('spawncar', function(source, args, rawCommand)
+    local x,y,z = table.unpack(GetOffsetFromEntityInWorldCoords(PlayerPedId(), 0.0, 8.0, 0.5))
+    local veh = args[1]
+    if veh == nil then veh = "adder" end
+    vehiclehash = GetHashKey(veh)
+    RequestModel(vehiclehash)
+    
+    Citizen.CreateThread(function() 
+        local waiting = 0
+        while not HasModelLoaded(vehiclehash) do
+            waiting = waiting + 100
+            Citizen.Wait(100)
+            if waiting > 5000 then
+                ShowNotification("~r~Vehicle not found.")
+                return
+            end
+        end
+        CreateVehicle(vehiclehash, x, y, z, GetEntityHeading(PlayerPedId())+90, 1, 0)
+    end)
+end)
